f81d9be495fca0a6f0cbecca0c2f564f4d2227ae
Dylan Dunne <dylanadunne@gmail.com>
2017-08-09 19:21:56 -0400

    successfully passing data throughout app

--- js/dataService.js
+++ js/dataService.js
@@ -1,5 +1,12 @@
 var app = angular.module("redditApp");
 
-app.factory("redditDataService", function() {
+app.service('redditDataService', function($http) {
+    this.getRedditData = function() {
+        var url = "https://www.reddit.com/r/aww/.json";
 
+        var promise = $http.get(url);
+
+        return promise;
+    }
 });
+
--- js/mainController.js
+++ js/mainController.js
@@ -1,5 +1,13 @@
 var app = angular.module("redditApp");
 
-app.controller("mainController", function($scope){
+app.controller("mainController", function($scope, redditDataService) {
+
+    $scope.cute = [];
+
+    var promise = redditDataService.getRedditData();
+    	promise.then(function(response) {
+        $scope.cute = response.data.data.children;
+        console.log($scope.cute);
+    });
 
 });
